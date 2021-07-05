3cc65f5a9f0a5a99968c37f864f558b7f07afcd3
Thanut <6110110184@psu.ac.th>
2021-07-05 19:21:45 +0700

    second

--- bin/digital_clock.dart
+++ bin/digital_clock.dart
@@ -1,7 +1,38 @@
+import 'dart:html';
 import 'dart:io';
 
-class Set {
-  int a = 0;
+abstract class Set {
+  List<int> setting(List<int> input);
+}
+
+class SetHour implements Set {
+  @override
+  List<int> setting(List<int> input) {
+    print('setting hour... ');
+    input[0] += 1;
+    return input;
+  }
+}
+
+class SetMin implements Set {
+  @override
+  List<int> setting(List<int> input) {
+    print('setting minite...');
+    input[1] += 1;
+    return input;
+  }
+}
+
+class Context {
+  List<int> time = [0, 0];
+  Set currentstate = new SetHour();
+  void setState(Set state) {
+    currentstate = state;
+  }
+
+  void inc() {
+    time = currentstate.setting(time);
+  }
 }
 
 void main(List<String> arguments) {
