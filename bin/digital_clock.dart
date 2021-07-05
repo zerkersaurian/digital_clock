22325949367c05af1e08da24ad7b67813eff1bc5
Thanut <6110110184@psu.ac.th>
2021-07-05 20:15:38 +0700

    second

--- bin/digital_clock.dart
+++ bin/digital_clock.dart
@@ -1,4 +1,3 @@
-import 'dart:html';
 import 'dart:io';
 
 abstract class Set {
@@ -8,8 +7,9 @@ abstract class Set {
 class SetHour implements Set {
   @override
   List<int> setting(List<int> input) {
-    print('setting hour... ');
+    //print('incresing 1 hour... ');
     input[0] += 1;
+    input[0] %= 24;
     return input;
   }
 }
@@ -17,15 +17,34 @@ class SetHour implements Set {
 class SetMin implements Set {
   @override
   List<int> setting(List<int> input) {
-    print('setting minite...');
+    // print('incresing 1 minite...');
     input[1] += 1;
+    if (input[1] / 60 >= 1) {
+      input[0] += input[1] ~/ 60;
+      input[1] %= 60;
+    }
+    return input;
+  }
+}
+
+class SetIdle implements Set {
+  @override
+  List<int> setting(List<int> input) {
+    //print('idling...');
     return input;
   }
 }
 
 class Context {
   List<int> time = [0, 0];
-  Set currentstate = new SetHour();
+  Context(this.time) {
+    time[0] %= 24;
+    if (time[1] ~/ 60 >= 1) {
+      time[0] += time[1] ~/ 60;
+      time[1] %= 60;
+    }
+  }
+  Set currentstate = SetIdle();
   void setState(Set state) {
     currentstate = state;
   }
@@ -33,9 +52,38 @@ class Context {
   void inc() {
     time = currentstate.setting(time);
   }
+
+  void telltime() {
+    print(time[0].toString() + ':' + time[1].toString());
+  }
 }
 
 void main(List<String> arguments) {
   var input = (stdin.readLineSync() ?? '').split(' ');
-  print(input);
+  List<int> timeinitial = [int.parse(input[1]), int.parse(input[2])];
+  Context c = new Context(timeinitial);
+  var set_type;
+  if (input[0] == 'on') {
+    var command = '';
+    if (timeinitial[0] % 24 == 0 && timeinitial[1] == 0) c.setState(SetHour());
+    while (true) {
+      command = stdin.readLineSync() ?? '';
+      if (command == 'out') {
+        break;
+      } else if (command == 'inc') {
+        c.inc();
+      } else if (command == 'set') {
+        set_type = c.currentstate.runtimeType.toString();
+        if (set_type == 'SetIdle') {
+          c.setState(SetHour());
+        } else if (set_type == 'SetMin') {
+          c.setState(SetIdle());
+        } else {
+          c.setState(SetMin());
+        }
+      } else if (command == 'tell') {
+        c.telltime();
+      }
+    }
+  }
 }
